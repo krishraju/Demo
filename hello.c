9f3bad239bb771bfeb014fbcd95e2af4edddf042
ramesh <ramesh.16786@gmail.com>
2013-10-13 09:27:02 +0530

    Dussera special amend gift..

--- hello.c
+++ hello.c
@@ -5,6 +5,11 @@ int main()
 printf("helloworld\n");
 printf("how are you ? \n");
 printf("have a good day \n");
+printf("Happy Dussera \n");
+printf("I am going to be positive from this vijay dashami,\
+ this our resolution \n");
+printf ("Be bold and brave \n");
+printf ("No one is great than you \n");
 printf("hi \n");
 return 0;
 }
